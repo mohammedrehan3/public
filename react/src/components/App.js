e885b894fd5d99cececece0e63f6ff4a73f368eb
mohammedrehan3 <rehanpasha182019@gmail.com>
2022-12-13 02:54:03 +0530

    reactproj

---
+++ react/src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Notes from "./Notes";
+import Footer from "./Footer";
+
+function App(){
+    return(
+        <div>
+            <Header />
+            <Notes />
+            <Notes />
+            <Notes />
+            <Footer />
+        </div>
+    );
+}
+export default App;
---
+++ react/src/components/Notes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notes(){
+    return(
+        <div class="note">
+        <h1>HTML</h1>
+            <p>HTML stands for Hyper Text Markup Language HTML is the standard markup language for creating Web pages HTML describes the structure of a Web page HTML consists of a series of elements HTML elements</p>
+        </div>
+    );
+}
+export default Notes;
